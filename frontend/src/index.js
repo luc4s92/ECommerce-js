1ca1d07393edc1ae7c51aa0a3dfec06552d88103
Lucas <lucas.acdc92@gmail.com>
2021-01-26 22:57:55 -0300

    Create the Product Screen -> get the product by id from the api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,22 @@
-import Error404Screen from './screens/Error404Screen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { parseRequestUrl } from './utils';
+import Error404Screen from './screens/Error404Screen';
 
 const routes = {
-    "/": HomeScreen,
-    "/product/:id": ProductScreen,
-}
-
-const router = async() => {
-
+    '/': HomeScreen,
+    '/product/:id': ProductScreen,
+};
+const router = async () => {
     const request = parseRequestUrl();
-    const parseUrl = (request.resource ? `/${request.resource}`: '/') + 
-    (request.id? '/:id': '') + 
-    (request.verb ? `/${request.verb}`: '');
-    const screen = routes[parseUrl] ? routes[parseUrl]: Error404Screen;
-    const main = document.getElementById("main-container");
+    const parseUrl =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id' : '') +
+    (request.verb ? `/${request.verb}` : '');
+    const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+
+    const main = document.getElementById('main-container');
     main.innerHTML = await screen.render();
 };
-
-window.addEventListener("load", router);
+window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
