15e6ca71fa8b0c4dd3f6c512148042d7e51dab07
Lucas <lucas.acdc92@gmail.com>
2021-01-27 22:47:29 -0300

    Style Product screen UI

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { parseRequestUrl } from '../utils';
 import { getProduct } from '../api';
+import Rating from '../components/Rating';
 
 const ProductScreen = {
     render: async () => {
@@ -8,7 +9,56 @@ const ProductScreen = {
     if (product.error) {
     return `<div>${product.error}</div>`;
     }
-    return `<h1>${product.name}</h1>`;
+    return `
+    <div class="content">
+        <div class="back-to-result">
+            <a href="/#/"> Back To Result</a>
+        </div>
+        <div class="details">
+            <div class="details-img">
+                <img src="${product.image}" alt="${product.name}" />
+            </div>
+            <div class="details-info">
+                <ul>
+                    <li>
+                        <h1>${product.name}</h1>
+                    </li>
+                    <li>${Rating.render({
+                        value: product.rating, 
+                        text: `${product.numReviews} reviews` 
+                    })} 
+                    </li>
+                    <li>
+                        Price: <strong>$ ${product.price} </strong>
+                    </li>
+                    
+                    <li>
+                        Description:
+                        <div>
+                            ${product.description}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="details-action">
+                <ul>
+                    <li>
+                        $${product.price}
+                    </li>
+                    <li>
+                        Status:
+                        ${product.countInStock > 0 
+                            ? `<span class="success"> In Stock </span>`
+                            : `<span class="error">Unavailable</span>`
+                    }
+                    </li>
+                    <li>
+                        <button id="add-button" class="primary fw"> Add To Cart </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div> `;
 },
 };
 export default ProductScreen;
