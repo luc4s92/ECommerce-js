8d2cf4abea23f67f7809416f7e6594c162c58f3b
Lucas <lucas.acdc92@gmail.com>
2021-02-12 21:33:08 -0300

    Create and style checkout wizard

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,5 @@
+import { getCartItems } from "./localStorage";
+
 export const parseRequestUrl = () => {
     const url = document.location.hash.toLowerCase();
     const request = url.split("/");
@@ -35,4 +37,12 @@ export const showMessage = (message, callback) =>{
             callback();
         }
     });
+};
+
+export const redirectUser = () =>{
+    if(getCartItems().length!== 0){
+        document.location.hash = '/shipping';
+    }else{
+        document.location.hash = '/';
+    }
 };
